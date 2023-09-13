19ae2e8d4ab5adc9d2fc6866809ca5d2c49b9ebe
sumin <cleonn7878@gmail.com>
2023-09-14 04:22:05 +0900

    Update BLE measurement feature

--- app/src/main/java/icsl/apps/collector/BLEModule.java
+++ app/src/main/java/icsl/apps/collector/BLEModule.java
@@ -16,10 +16,16 @@ import android.util.Log;
 import android.util.SparseArray;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
+import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 
 public class BLEModule {
     private String TAG = "BLE_MODULE";
+    private static final int APPLE = 0X004C;
+    private static final int TYPE_BEACON = 0X0002;
     private boolean flag_is_ble_running = false;
     private long measurement_start_time_ms;
     private int count;
@@ -47,49 +53,37 @@ public class BLEModule {
                 float elapsed_app_time_s = (float) (elapsedRealtime() / 1e3 - measurement_start_time_ms / 1e3);
                 ScanRecord scanRecord = result.getScanRecord();
                 if (scanRecord != null) {
-                    byte[] mBytes = scanRecord.getBytes();
-                    if (mBytes != null) {
-                        StringBuilder sb = new StringBuilder();
-                        for (byte b : mBytes) {
-                            sb.append(String.format("%02X",b));
-                            file.save_str_to_file(String.format("%02X",b) + " / ");
-                        }
-                        Log.d(TAG, sb.toString());
-                        file.save_str_to_file("\n");
-//                    file.save_str_to_file(sb.toString() + "\n");
-                    }
-//                String scanResult;
-                    byte[] imanufactureData =  scanRecord.getManufacturerSpecificData(0X004C);
-
-                    if (imanufactureData != null && imanufactureData.length > 23) {
+                    // check is iBeacon
+                    byte[] manufacture_data_apple =  scanRecord.getManufacturerSpecificData(APPLE);
+                    if (manufacture_data_apple != null && manufacture_data_apple[0] == TYPE_BEACON) {
                         count_iBeacon++;
-                        last_ble_value = "BLE4;" + elapsed_app_time_s + ";" + result.getDevice().getAddress() + "; "
-                                + "iBeacon;" + result.getDevice().getName() + "; " + result.getRssi() + ";" + imanufactureData.toString() + ";";
-//                    Log.d(TAG, scanResult);
-//                    if (result.getDevice().getType() == 0X02)
-//                        Log.d(TAG, result.getDevice().getType() + "");
-//                    scanRec
+                        byte[] arr_uuid = Arrays.copyOfRange(manufacture_data_apple,2,18);
+                        byte[] arr_major_ID = Arrays.copyOfRange(manufacture_data_apple,18,20);
+                        byte[] arr_minor_ID = Arrays.copyOfRange(manufacture_data_apple,20,22);
 
+                        UUID uuid = get_iBeacon_uuid(arr_uuid);
+                        int major_ID = Integer.parseInt(bytes_to_hex_string(arr_major_ID));
+                        int minor_ID = Integer.parseInt(bytes_to_hex_string(arr_minor_ID));
+                        last_ble_value = String.format("BLE4, %f, iBeacon, %s, %d, %d, %d, %d, %s",elapsed_app_time_s, result.getDevice().getAddress()
+                                                        , result.getRssi(),scanRecord.getTxPowerLevel(),major_ID,minor_ID,uuid);
+                        Log.d(TAG,scanRecord.getDeviceName() + "");
+                        Log.d(TAG,result.getDevice().getName() + "");
                     }
-//                Log.d(TAG,result.toString());
-//                Log.d(TAG,scanRecord.)
-                    if (scanRecord.getServiceUuids() != null) {
-                        String uuids = "";
-                        for (int i=0; i<scanRecord.getServiceUuids().size(); i++) {
-                            uuids += scanRecord.getServiceUuids().get(i).toString() + " / ";
-                        }
-                        if (scanRecord.getServiceUuids().get(0).toString().equals("0000FEAA-0000–1000–8000–00805F9B34FB"))
-                            count_Eddystone++;
-//                    Log.d(TAG,uuids);
-                        uuids = scanRecord.getServiceUuids().get(0).toString();
-                        last_ble_value += uuids + ";";
-                    }
-                    last_ble_value += "\n";
-                    String str_status = String.format("Count: %d\t\tiBeacon: %d\t\tEddyStone: %d",count, count_iBeacon, count_Eddystone);
+//                    if (scanRecord.getServiceUuids() != null) {
+//                        String uuids = "";
+//                        for (int i=0; i<scanRecord.getServiceUuids().size(); i++) {
+//                            uuids += scanRecord.getServiceUuids().get(i).toString() + " / ";
+//                        }
+//                        if (scanRecord.getServiceUuids().get(0).toString().equals("0000FEAA-0000–1000–8000–00805F9B34FB"))
+//                            count_Eddystone++;
+////                    Log.d(TAG,uuids);
+//                        uuids = scanRecord.getServiceUuids().get(0).toString();
+//                    }
+                    String str_status = String.format("Count: %d\t\tiBeacon: %d",count, count_iBeacon);
                     mListener.ble_status(str_status, MeasurementListener.TYPE_BLE_STATUS);
-                    mListener.ble_status(scanRecord + result.getDevice().getAddress() + "\n", MeasurementListener.TYPE_BLE_VALUE);
+                    mListener.ble_status(last_ble_value + "\n", MeasurementListener.TYPE_BLE_VALUE);
                     if (file != null) {
-//                    file.save_str_to_file(last_ble_value + "\n");
+                        file.save_str_to_file(last_ble_value + "\n");
                     }
                 }
             }
@@ -126,5 +120,21 @@ public class BLEModule {
     public void stop_measurement() {
         bleScanner.stopScan(scanCallback);
         flag_is_ble_running = false;
+    }
+    @NonNull
+    private String bytes_to_hex_string(@NonNull byte[] mBytes) {
+            StringBuilder sb = new StringBuilder();
+            for (byte b : mBytes) {
+                sb.append(String.format("%02X", b));
+            }
+            return sb.toString();
+    }
+    private UUID get_iBeacon_uuid(byte[] mBytes) {
+        String str_uuid = bytes_to_hex_string(mBytes);
+        String str_iBeacon_uuid = str_uuid.substring(0, 8) + "-" + str_uuid.substring(8, 12) + "-"
+                                + str_uuid.substring(12, 16) + "-" + str_uuid.substring(16, 20) + "-"
+                                + str_uuid.substring(20);
+        UUID uuid = UUID.fromString(str_iBeacon_uuid);
+        return uuid;
     }
 }
