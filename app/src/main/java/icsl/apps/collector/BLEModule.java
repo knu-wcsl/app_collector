a9d7fe1da5260319366d285d4e10dbbb3b1d6a55
sumin <cleonn7878@gmail.com>
2023-09-13 22:23:58 +0900

    Update BLE measurement

--- app/src/main/java/icsl/apps/collector/BLEModule.java
+++ app/src/main/java/icsl/apps/collector/BLEModule.java
@@ -31,7 +31,6 @@ public class BLEModule {
     private FileModule file;
     private BluetoothManager bluetoothManager;
     private BluetoothAdapter bluetoothAdapter;
-    private BluetoothAdapter.LeScanCallback leScanCallback;
     private BluetoothLeScanner bleScanner;
     private ScanCallback scanCallback;
     public BLEModule(Activity activity, MeasurementListener listener) {
@@ -39,19 +38,6 @@ public class BLEModule {
         mListener = listener;
         bluetoothManager = (BluetoothManager) mActivity.getApplicationContext().getSystemService(Context.BLUETOOTH_SERVICE);
         bluetoothAdapter = bluetoothManager.getAdapter();
-        leScanCallback = new BluetoothAdapter.LeScanCallback() {
-            @Override
-            public void onLeScan(BluetoothDevice device, int rssi, byte[] bytes) {
-                if (device.getName() != null || device.getName() == null) {
-                    float elapsed_app_time_s = (float) (elapsedRealtime() / 1e3 - measurement_start_time_ms / 1e3);
-//                    float elapsed_sensor_fw_time_s = (float) (event.timestamp / 1e9 - measurement_start_time_ms / 1e3);
-                    String scanResult = "BLE, " + device.getName() + ", " + device.getAddress() + ", " + rssi + "\n";
-                    Log.d(TAG,scanResult);
-                    if (file != null)
-                        file.save_str_to_file(scanResult);
-                }
-            }
-        };
         bleScanner = bluetoothAdapter.getBluetoothLeScanner();
         scanCallback = new ScanCallback() {
             @Override
@@ -60,30 +46,51 @@ public class BLEModule {
                 super.onScanResult(callbackType, result);
                 float elapsed_app_time_s = (float) (elapsedRealtime() / 1e3 - measurement_start_time_ms / 1e3);
                 ScanRecord scanRecord = result.getScanRecord();
-//                String scanResult;
-                SparseArray<byte[]> manufactureData =  scanRecord.getManufacturerSpecificData();
-                if (manufactureData.size() != 0 && manufactureData.keyAt(0) == 0X4C) {
-                    count_iBeacon++;
-                    last_ble_value = "BLE4;" + elapsed_app_time_s + ";" + result.getDevice().getAddress() + "; "
-                            + "iBeacon;" + result.getDevice().getName() + "; " + result.getRssi() + ";" + manufactureData.toString() + ";\n";
-//                    Log.d(TAG, scanResult);
-                }
-                if (scanRecord.getServiceUuids() != null) {
-                    String uuids = "";
-                    for (int i=0; i<scanRecord.getServiceUuids().size(); i++) {
-                        uuids += scanRecord.getServiceUuids().get(i).toString() + " / ";
+                if (scanRecord != null) {
+                    byte[] mBytes = scanRecord.getBytes();
+                    if (mBytes != null) {
+                        StringBuilder sb = new StringBuilder();
+                        for (byte b : mBytes) {
+                            sb.append(String.format("%02X",b));
+                            file.save_str_to_file(String.format("%02X",b) + " / ");
+                        }
+                        Log.d(TAG, sb.toString());
+                        file.save_str_to_file("\n");
+//                    file.save_str_to_file(sb.toString() + "\n");
                     }
-                    if (scanRecord.getServiceUuids().get(0).toString().equals("0000FEAA-0000–1000–8000–00805F9B34FB"))
-                        count_Eddystone++;
+//                String scanResult;
+                    byte[] imanufactureData =  scanRecord.getManufacturerSpecificData(0X004C);
+
+                    if (imanufactureData != null && imanufactureData.length > 23) {
+                        count_iBeacon++;
+                        last_ble_value = "BLE4;" + elapsed_app_time_s + ";" + result.getDevice().getAddress() + "; "
+                                + "iBeacon;" + result.getDevice().getName() + "; " + result.getRssi() + ";" + imanufactureData.toString() + ";";
+//                    Log.d(TAG, scanResult);
+//                    if (result.getDevice().getType() == 0X02)
+//                        Log.d(TAG, result.getDevice().getType() + "");
+//                    scanRec
+
+                    }
+//                Log.d(TAG,result.toString());
+//                Log.d(TAG,scanRecord.)
+                    if (scanRecord.getServiceUuids() != null) {
+                        String uuids = "";
+                        for (int i=0; i<scanRecord.getServiceUuids().size(); i++) {
+                            uuids += scanRecord.getServiceUuids().get(i).toString() + " / ";
+                        }
+                        if (scanRecord.getServiceUuids().get(0).toString().equals("0000FEAA-0000–1000–8000–00805F9B34FB"))
+                            count_Eddystone++;
 //                    Log.d(TAG,uuids);
-                }
-                String str_status = String.format("Count: %d\t\tiBecon count %d\t\tEddyStone count %d",count, count_iBeacon, count_Eddystone);
-                mListener.ble_status(str_status, MeasurementListener.TYPE_BLE_STATUS);
-//                if (scanResult != null)
-                mListener.ble_status(scanRecord + result.getDevice().getAddress() + "\n", MeasurementListener.TYPE_BLE_VALUE);
-                if (file != null) {
-//                    Log.d(TAG, "Im ble save!");
-                    file.save_str_to_file(last_ble_value + "\n");
+                        uuids = scanRecord.getServiceUuids().get(0).toString();
+                        last_ble_value += uuids + ";";
+                    }
+                    last_ble_value += "\n";
+                    String str_status = String.format("Count: %d\t\tiBeacon: %d\t\tEddyStone: %d",count, count_iBeacon, count_Eddystone);
+                    mListener.ble_status(str_status, MeasurementListener.TYPE_BLE_STATUS);
+                    mListener.ble_status(scanRecord + result.getDevice().getAddress() + "\n", MeasurementListener.TYPE_BLE_VALUE);
+                    if (file != null) {
+//                    file.save_str_to_file(last_ble_value + "\n");
+                    }
                 }
             }
             @Override
@@ -107,7 +114,6 @@ public class BLEModule {
             return false;
         }
         measurement_start_time_ms = start_time_ms;
-//        bluetoothAdapter.startLeScan(leScanCallback);
         count = 0;
         count_iBeacon = 0;
         count_Eddystone = 0;
@@ -118,7 +124,6 @@ public class BLEModule {
     }
 
     public void stop_measurement() {
-//        bluetoothAdapter.stopLeScan(leScanCallback);
         bleScanner.stopScan(scanCallback);
         flag_is_ble_running = false;
     }
